f52db6acdddc06ede9989f34d5860e17ca56703c
Soheil <sohail_nader@yahoo.com>
2020-07-30 15:24:17 +0430

    Update Dice.py

--- Dice/Dice.py
+++ Dice/Dice.py
@@ -1,11 +1,56 @@
 #By: Soheil
 import random
-dice=['ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX']
 y=True
 while(y):
     x = input("Press enter to roll dice, if you wanna quit, type \"quit\": ")
+    rnd = random.choice(range(1,7))
     if x == 'quit':
         y=False
     else:
-        print(random.choice(dice))
+        print(rnd)
+        if rnd == 6:
+            print("""
+#########
+# 0   0 #
+# 0   0 #
+# 0   0 #
+#########""")
 
+        if rnd == 5:
+            print("""
+#########
+# 0   0 #
+#   0   #
+# 0   0 #
+#########""")
+        if rnd == 4:
+            print("""
+#########
+# 0   0 #
+#       #
+# 0   0 #
+#########""")
+
+        if rnd == 3:
+            print("""
+#########
+#     0 #
+#   0   #
+# 0     #
+#########""")
+
+        if rnd == 2:
+            print("""
+#########
+#     0 #
+#       #
+# 0     #
+#########""")
+
+        if rnd == 1:
+            print("""
+#########
+#       #
+#   0   #
+#       #
+#########""")
